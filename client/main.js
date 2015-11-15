3bc246baa2ac2aedef4bcea4a5079758b4b1d44e
Nic Aitch <nic@nicinabox.com>
2015-11-15 01:59:23 -0600

    Add highlight to slot

---
+++ client/main.js
@@ -0,0 +1,54 @@
+var STORAGE_KEY = 'nodevember-sechedule-selections'
+var timeSlots = document.querySelectorAll('.js-slot')
+
+var getSelections = function() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+}
+
+var getSelectionById = function(id) {
+  var selections = getSelections()
+  return selections.filter(function(s) {
+    return s.id === id
+  })[0]
+}
+
+var getSelectionIndex = function(id, all) {
+  return all.map(function(s) { return s.id }).indexOf(id)
+}
+
+var saveSelection = function(id, isSelected) {
+  var selections = getSelections()
+  var index = getSelectionIndex(id, selections)
+
+  if (index > -1) {
+    selections[index].selected = isSelected
+  } else {
+    selections = selections.concat({
+      id: id,
+      selected: isSelected
+    })
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(selections))
+}
+
+var applySelections = function() {
+  var selections = getSelections()
+  selections.forEach(function(selection) {
+    var el
+    if (selection.selected) {
+      el = document.getElementById(selection.id)
+      el.classList.toggle('info')
+    }
+  })
+}
+
+applySelections()
+
+Array.prototype.forEach.call(timeSlots, function(el) {
+  el.addEventListener('click', function(e) {
+    var t = e.currentTarget
+    t.classList.toggle('info')
+    saveSelection(t.id, t.classList.contains('info'))
+  })
+})
